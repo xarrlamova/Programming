7914f464b4e5bb56a10d03f80aef6333c93c4379
Алиса Харламова <xarlamova_alisa1@outlook.com>
2023-03-31 20:31:16 +0700

    feauture: добавлены комментарии

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -33,15 +33,15 @@
             this.EnumsTabPage = new System.Windows.Forms.TabPage();
             this.TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.TableLayoutPanelChild = new System.Windows.Forms.TableLayoutPanel();
-            this.EnumerationsGroupBox = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.IntValueLabel = new System.Windows.Forms.Label();
-            this.ChooseValueLabel = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.ChooseEnumerationLabel = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.WeekdayParsingGroupBox = new System.Windows.Forms.GroupBox();
             this.SeasonHandleGroupBox = new System.Windows.Forms.GroupBox();
+            this.EnumerationsGroupBox = new System.Windows.Forms.GroupBox();
+            this.ValueIntTextBox = new System.Windows.Forms.TextBox();
+            this.IntValueLabel = new System.Windows.Forms.Label();
+            this.ChooseValueLabel = new System.Windows.Forms.Label();
+            this.EnumValuesListBox = new System.Windows.Forms.ListBox();
+            this.ChooseEnumerationLabel = new System.Windows.Forms.Label();
+            this.AllEnumsListBox = new System.Windows.Forms.ListBox();
             this.EnumsTabControl.SuspendLayout();
             this.EnumsTabPage.SuspendLayout();
             this.TableLayoutPanel.SuspendLayout();
@@ -103,80 +103,6 @@
             this.TableLayoutPanelChild.Size = new System.Drawing.Size(568, 214);
             this.TableLayoutPanelChild.TabIndex = 0;
             // 
-            // EnumerationsGroupBox
-            // 
-            this.EnumerationsGroupBox.Controls.Add(this.textBox1);
-            this.EnumerationsGroupBox.Controls.Add(this.IntValueLabel);
-            this.EnumerationsGroupBox.Controls.Add(this.ChooseValueLabel);
-            this.EnumerationsGroupBox.Controls.Add(this.listBox2);
-            this.EnumerationsGroupBox.Controls.Add(this.ChooseEnumerationLabel);
-            this.EnumerationsGroupBox.Controls.Add(this.listBox1);
-            this.EnumerationsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.EnumerationsGroupBox.Location = new System.Drawing.Point(3, 3);
-            this.EnumerationsGroupBox.Name = "EnumerationsGroupBox";
-            this.EnumerationsGroupBox.Size = new System.Drawing.Size(562, 207);
-            this.EnumerationsGroupBox.TabIndex = 1;
-            this.EnumerationsGroupBox.TabStop = false;
-            this.EnumerationsGroupBox.Text = "Enumerations";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(334, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(136, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // IntValueLabel
-            // 
-            this.IntValueLabel.AutoSize = true;
-            this.IntValueLabel.Location = new System.Drawing.Point(334, 24);
-            this.IntValueLabel.Name = "IntValueLabel";
-            this.IntValueLabel.Size = new System.Drawing.Size(55, 15);
-            this.IntValueLabel.TabIndex = 7;
-            this.IntValueLabel.Text = "Int value:";
-            // 
-            // ChooseValueLabel
-            // 
-            this.ChooseValueLabel.AutoSize = true;
-            this.ChooseValueLabel.Location = new System.Drawing.Point(170, 24);
-            this.ChooseValueLabel.Name = "ChooseValueLabel";
-            this.ChooseValueLabel.Size = new System.Drawing.Size(81, 15);
-            this.ChooseValueLabel.TabIndex = 3;
-            this.ChooseValueLabel.Text = "Choose value:";
-            // 
-            // listBox2
-            // 
-            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.IntegralHeight = false;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(170, 42);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(136, 149);
-            this.listBox2.TabIndex = 2;
-            // 
-            // ChooseEnumerationLabel
-            // 
-            this.ChooseEnumerationLabel.AutoSize = true;
-            this.ChooseEnumerationLabel.Location = new System.Drawing.Point(6, 24);
-            this.ChooseEnumerationLabel.Name = "ChooseEnumerationLabel";
-            this.ChooseEnumerationLabel.Size = new System.Drawing.Size(121, 15);
-            this.ChooseEnumerationLabel.TabIndex = 1;
-            this.ChooseEnumerationLabel.Text = "Choose enumeration:";
-            // 
-            // listBox1
-            // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.IntegralHeight = false;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(6, 42);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(136, 149);
-            this.listBox1.TabIndex = 0;
-            // 
             // WeekdayParsingGroupBox
             // 
             this.WeekdayParsingGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -197,6 +123,82 @@
             this.SeasonHandleGroupBox.TabStop = false;
             this.SeasonHandleGroupBox.Text = "Season Handle";
             // 
+            // EnumerationsGroupBox
+            // 
+            this.EnumerationsGroupBox.Controls.Add(this.ValueIntTextBox);
+            this.EnumerationsGroupBox.Controls.Add(this.IntValueLabel);
+            this.EnumerationsGroupBox.Controls.Add(this.ChooseValueLabel);
+            this.EnumerationsGroupBox.Controls.Add(this.EnumValuesListBox);
+            this.EnumerationsGroupBox.Controls.Add(this.ChooseEnumerationLabel);
+            this.EnumerationsGroupBox.Controls.Add(this.AllEnumsListBox);
+            this.EnumerationsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EnumerationsGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.EnumerationsGroupBox.Name = "EnumerationsGroupBox";
+            this.EnumerationsGroupBox.Size = new System.Drawing.Size(562, 207);
+            this.EnumerationsGroupBox.TabIndex = 1;
+            this.EnumerationsGroupBox.TabStop = false;
+            this.EnumerationsGroupBox.Text = "Enumerations";
+            // 
+            // ValueIntTextBox
+            // 
+            this.ValueIntTextBox.Location = new System.Drawing.Point(334, 42);
+            this.ValueIntTextBox.Name = "ValueIntTextBox";
+            this.ValueIntTextBox.Size = new System.Drawing.Size(136, 23);
+            this.ValueIntTextBox.TabIndex = 0;
+            // 
+            // IntValueLabel
+            // 
+            this.IntValueLabel.AutoSize = true;
+            this.IntValueLabel.Location = new System.Drawing.Point(334, 24);
+            this.IntValueLabel.Name = "IntValueLabel";
+            this.IntValueLabel.Size = new System.Drawing.Size(55, 15);
+            this.IntValueLabel.TabIndex = 7;
+            this.IntValueLabel.Text = "Int value:";
+            // 
+            // ChooseValueLabel
+            // 
+            this.ChooseValueLabel.AutoSize = true;
+            this.ChooseValueLabel.Location = new System.Drawing.Point(170, 24);
+            this.ChooseValueLabel.Name = "ChooseValueLabel";
+            this.ChooseValueLabel.Size = new System.Drawing.Size(81, 15);
+            this.ChooseValueLabel.TabIndex = 3;
+            this.ChooseValueLabel.Text = "Choose value:";
+            // 
+            // EnumValuesListBox
+            // 
+            this.EnumValuesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.EnumValuesListBox.FormattingEnabled = true;
+            this.EnumValuesListBox.IntegralHeight = false;
+            this.EnumValuesListBox.ItemHeight = 15;
+            this.EnumValuesListBox.Location = new System.Drawing.Point(170, 42);
+            this.EnumValuesListBox.Name = "EnumValuesListBox";
+            this.EnumValuesListBox.Size = new System.Drawing.Size(136, 149);
+            this.EnumValuesListBox.TabIndex = 2;
+            this.EnumValuesListBox.SelectedIndexChanged += new System.EventHandler(this.EnumValuesListBox_SelectedIndexChanged);
+            // 
+            // ChooseEnumerationLabel
+            // 
+            this.ChooseEnumerationLabel.AutoSize = true;
+            this.ChooseEnumerationLabel.Location = new System.Drawing.Point(6, 24);
+            this.ChooseEnumerationLabel.Name = "ChooseEnumerationLabel";
+            this.ChooseEnumerationLabel.Size = new System.Drawing.Size(121, 15);
+            this.ChooseEnumerationLabel.TabIndex = 1;
+            this.ChooseEnumerationLabel.Text = "Choose enumeration:";
+            // 
+            // AllEnumsListBox
+            // 
+            this.AllEnumsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.AllEnumsListBox.FormattingEnabled = true;
+            this.AllEnumsListBox.IntegralHeight = false;
+            this.AllEnumsListBox.ItemHeight = 15;
+            this.AllEnumsListBox.Location = new System.Drawing.Point(6, 42);
+            this.AllEnumsListBox.Name = "AllEnumsListBox";
+            this.AllEnumsListBox.Size = new System.Drawing.Size(136, 149);
+            this.AllEnumsListBox.TabIndex = 0;
+            this.AllEnumsListBox.SelectedIndexChanged += new System.EventHandler(this.AllEnumsListBox_SelectedIndexChanged);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -206,6 +208,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Programming Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.EnumsTabControl.ResumeLayout(false);
             this.EnumsTabPage.ResumeLayout(false);
             this.TableLayoutPanel.ResumeLayout(false);
@@ -224,11 +227,11 @@
         private TableLayoutPanel TableLayoutPanelChild;
         private GroupBox EnumerationsGroupBox;
         private Label ChooseEnumerationLabel;
-        private ListBox listBox1;
+        private ListBox AllEnumsListBox;
         private Label IntValueLabel;
         private Label ChooseValueLabel;
-        private ListBox listBox2;
-        private TextBox textBox1;
+        private ListBox EnumValuesListBox;
+        private TextBox ValueIntTextBox;
         private GroupBox WeekdayParsingGroupBox;
         private GroupBox SeasonHandleGroupBox;
     }
--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -16,6 +16,44 @@ namespace Programming.View
         public MainForm()
         {
             InitializeComponent();
+
+            object[] enums = new object[] { typeof(Color), typeof(EducationForm), typeof(Genre), typeof(Manufactures), typeof(Season), typeof(Weekday) };
+            //создаем массив с названиями пееречислений
+            AllEnumsListBox.Items.AddRange(enums); //передаем массив со значениями в AllEnumsListBox
+        }
+
+        private void AllEnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedEnum = (Type)AllEnumsListBox.SelectedItem;
+            //выбранное значение - то, что кликнул пользователь
+
+            var enumValues = Enum.GetValues(selectedEnum);
+            //создаем массив enumValues со значениями перечисления
+            EnumValuesListBox.Items.Clear();
+            //Очищаем EnumValuesListBox от прошлых перечислений
+            foreach (var enumValue in enumValues)
+            {
+                EnumValuesListBox.Items.Add(enumValue);
+            }
+            //добавляем значения перечислений в EnumValuesListBox
+        }
+
+        private void EnumValuesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var selectedEnum = (Type)AllEnumsListBox.Items[0];
+            //выбранное значение по умолчанию - 0
+            var enumValues = Enum.GetValues(selectedEnum);
+            //массив со значениями перечисления
+            foreach (var enumValue in Enum.GetValues(selectedEnum))
+            {
+                EnumValuesListBox.Items.Add(enumValue);
+            }
+            //добавляем значения в EnumValuesListBox
         }
     }
 }
